503cf6af75409265e95ed30bdbebab270ec576be
jamison05 <joshua.jamison2@gmail.com>
2018-01-06 23:49:46 -0500

    Beginning use of handler
    Since background services have recently been taken out, I may have to move to handlers to perform the operations for RESTful requests.

---
+++ src/main/java/com/preludediscovery/filter6_soundandopengles/Download_files_collections.java
@@ -0,0 +1,32 @@
+package com.preludediscovery.filter6_soundandopengles;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import java.net.URL;
+
+/**
+ * Created by deepj on 1/6/2018.
+ */
+
+public class Download_files_collections extends MainActivity {
+Handler mHandler;
+    // Defines a Handler object that's attached to the UI thread
+
+
+
+
+
+        void initialize_handler (){
+        mHandler = new Handler(Looper.getMainLooper());
+
+        }
+
+
+
+
+
+
+}
